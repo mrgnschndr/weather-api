65c7844f9528a64c605c48b3b450d5ef7e9f924d
mrgnschndr <mrgnschndr@gmail.com>
2024-12-03 16:34:18 -0700

    Added main description of weather now in Canton

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ class App extends React.Component {
         super(props);
         this.state = {
             lat: null,
-            lon: null
+            lon: null,
+            main: null
         }
     }
     
     componentDidMount() {
         this.getCoordinatesWithFetch();
+        
     }
+
+    // componentDidUpdate() {
+    //     this.getWeatherWithFetch();
+    // }
 
 
     // fetch 
@@ -39,6 +45,53 @@ class App extends React.Component {
             console.log(error.message)
         }
 
+      //   url = "http://api.openweathermap.org/data/2.5/forecast";
+      // // API call: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+      
+
+      //   try {
+      //       let res = await fetch(`${url}?lat=${this.state.lat}&lon=${this.state.lon}&appid=${apiKey}`);
+      //       console.log('Weather res:', res);
+
+      //       if (!res.ok) {
+      //         throw new Error(`Error: ${res.status} - ${res.statusText}`)
+      //       }
+
+      //       let data = await res.json();
+      //       console.log(data);
+      //       this.setState({
+      //         main: data.list[0].main
+      //       }
+      //     )
+      //   } catch (error) {
+      //     console.log(error.message);
+      //   }
+      this.getWeatherWithFetch();
+    }
+
+    getWeatherWithFetch = async () => {
+      let apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+      let url = "http://api.openweathermap.org/data/2.5/forecast";
+      // API call: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+      try {
+          let res = await fetch(`${url}?lat=${this.state.lat}&lon=${this.state.lon}&appid=${apiKey}`);
+          console.log('Weather res:', res);
+
+          if (!res.ok) {
+            throw new Error(`Error: ${res.status} - ${res.statusText}`)
+          }
+
+          let data = await res.json();
+          console.log(data);
+          this.setState({
+            main: data.list[0].weather[0].main
+          }
+        )
+      } catch (error) {
+        console.log(error.message);
+      }
+
     }
 
     
@@ -48,6 +101,7 @@ class App extends React.Component {
                 <h1>App</h1>
                 <p>{`API KEY: ${process.env.REACT_APP_WEATHER_API_KEY}`}</p>
                 <p>{`Lat: ${this.state.lat}, Lon: ${this.state.lon}`}</p>
+                <p>{`Weather Now: ${this.state.main}`}</p>
             </div>
         )
     }
