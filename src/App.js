72035323b9ff1f2493a44ec2365c55aa919e6fd0
mrgnschndr <mrgnschndr@gmail.com>
2024-12-03 15:49:43 -0700

    initial commit from class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: null,
+            lon: null
+        }
+    }
+    
+    componentDidMount() {
+        this.getCoordinatesWithFetch();
+    }
+
+
+    // fetch 
+    getCoordinatesWithFetch = async () => {
+        let apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+        let city = "Canton";
+        let url = "http://api.openweathermap.org/geo/1.0/direct";
+
+        try {
+            let res = await fetch(`${url}?q=${city}&appid=${apiKey}`);
+            console.log(res);
+
+            if (!res.ok) {
+                throw new Error(`Error: ${res.status} - ${res.statusText}`)
+            }
+
+            let data = await res.json();
+            console.log(data);
+            this.setState({ 
+                lat: data[0].lat, 
+                lon: data[0].lon
+            })
+        } catch (error) {
+            console.log(error.message)
+        }
+
+    }
+
+    
+    render() {
+        return (
+            <div className="App">
+                <h1>App</h1>
+                <p>{`API KEY: ${process.env.REACT_APP_WEATHER_API_KEY}`}</p>
+                <p>{`Lat: ${this.state.lat}, Lon: ${this.state.lon}`}</p>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
